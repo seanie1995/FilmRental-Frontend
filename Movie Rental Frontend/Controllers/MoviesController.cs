d6ca3e80a51ca88b9baecfdaaf75cfa39ea345fd
Sean Schelin <119659530+seanie1995@users.noreply.github.com>
2024-09-04 12:07:12 +0200

    Add project files.

---
+++ Movie Rental Frontend/Controllers/MoviesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movie_Rental_Frontend.Models;
+using Newtonsoft.Json;
+
+namespace Movie_Rental_Frontend.Controllers
+{
+	public class MoviesController : Controller
+	{
+		private readonly HttpClient _httpClient;
+		private string baseUri = "http://localhost:7127/";
+
+		public MoviesController(HttpClient client)
+        {
+            _httpClient = client;
+        }
+        public async Task<IActionResult> Index()
+		{
+			ViewData["Title"] = "Available Movies";
+			
+			var response = await _httpClient.GetAsync($"{baseUri}/api/Movies");
+
+			var json = await response.Content.ReadAsStringAsync();
+
+			var movieList = JsonConvert.DeserializeObject<List<Movie>>(json);
+
+			return View(movieList);
+		}
+	}
+}
---
+++ Movie Rental Frontend/Models/Movie.cs
@@ -0,0 +1,9 @@
+﻿namespace Movie_Rental_Frontend.Models
+{
+	public class Movie
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public int ReleaseYear { get; set; }
+	}
+}
