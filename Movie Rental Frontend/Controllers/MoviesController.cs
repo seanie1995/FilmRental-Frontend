f7a99639acc9f0f69ec41debce1d72e994249ef3
Sean Schelin <119659530+seanie1995@users.noreply.github.com>
2024-09-04 13:49:30 +0200

    Added functioning code.

--- Movie Rental Frontend/Controllers/MoviesController.cs
+++ Movie Rental Frontend/Controllers/MoviesController.cs
@@ -1,29 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Movie_Rental_Frontend.Models;
-using Newtonsoft.Json;
 
-namespace Movie_Rental_Frontend.Controllers
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MovieRentalFrontend.Controllers
 {
 	public class MoviesController : Controller
 	{
-		private readonly HttpClient _httpClient;
-		private string baseUri = "http://localhost:7127/";
-
+		private readonly HttpClient _client;
+		private string baseUri = "https://localhost:7127/";
 		public MoviesController(HttpClient client)
-        {
-            _httpClient = client;
-        }
-        public async Task<IActionResult> Index()
+		{
+			_client = client;
+		}
+
+		public async Task<IActionResult> Index()
 		{
 			ViewData["Title"] = "Available Movies";
-			
-			var response = await _httpClient.GetAsync($"{baseUri}/api/Movies");
 
+			// Anropar APIets endpoint för att hämta alla filmer
+			var response = await _client.GetAsync($"{baseUri}api/Movies");
+
+			// läser av JSON som en string från bodyn
 			var json = await response.Content.ReadAsStringAsync();
 
+			// Omvandlar JSON till ett objekt av typen List<Movie>();
 			var movieList = JsonConvert.DeserializeObject<List<Movie>>(json);
 
+			// returnera listan till vyn som tar emot det för att arbeta med datan.
 			return View(movieList);
 		}
+
+		
 	}
 }
